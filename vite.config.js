dbac0f5e56f8eee8402d49e9899490dd2ed3d9ac
Gustavo Freire <gfreire@age-se.com>
2026-06-24 10:53:39 -0300

    update vite config base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/AG-E-QC-Checklist/',
+  base: '/AGE-QC-Checklist/',
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,
+  },
 })
